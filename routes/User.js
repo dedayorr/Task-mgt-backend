5336cd8e0f6796e2d0bf5a52a128d8a9e52a7f6c
dedayorr <dedayo16@gmail.com>
2023-04-28 12:21:00 +0100

    server

---
+++ routes/User.js
@@ -0,0 +1,8 @@
+// import express
+const express = require("express")
+
+// invoke the express router function
+const router = express.Router();
+
+
+export default router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const Task = require("./models/TaskSchema");
 //middleware
 app.use(express.json());
 
-//routes
+//create task
 app.post("/create", async (req, res) => {
   try {
     const { title, desc, date } = req.body;
@@ -28,10 +28,50 @@ app.post("/create", async (req, res) => {
 });
 
 // read task
+app.get("/tasks", async (req, res)=>{
+    try {
+        const allTask = await Task.find()
+        res.status(200).json(allTask)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+})
+
+//get one task
+app.get("/tasks/:id", async (req,res)=>{
+    const {id} = req.params;
+    try {
+        const oneTask = await Task.findById({_id:id})
+        res.status(200).json(oneTask)
+    } catch (error) {
+        res.status(400).json(error.message)
+    }
+})
+
+//delete task
+app.delete("/tasks/:id", async (req, res)=>{
+const {id} = req.params;
+try {
+    const deleteTask = await Task.findByIdAndDelete({_id:id})
+    res.status(200).json("deleted")
+} catch (error) {
+    res.status(400).json(error.message)
+}
+})
+
+//update task
+app.put("/tasks/:id", async (req, res)=>{
+const {id} = req.params;
+try {
+    const updateTask = await Task.findByIdAndUpdate(id, {$set: req.body })
+    res.status(200).json("updated")
+} catch (error) {
+    res.status(400).json(error.message)
+}
+})
 
 
-
-// create port
+//port to listen
 const port = process.env.PORT || 5005;
 
 dbConnect()
